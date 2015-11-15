8a3ab4370417d5abc9274c08bbe1507dc6924ef2
silviuapostu <silviu.apostu@gmail.com>
2015-11-15 22:20:28 +0100

    solved tasks for p=1

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+J = (sumsq(X * theta - y) + lambda * sumsq(theta(2:end))) /2/m;
 
 
-
-
-
-
-
-
-
-
+grad = 1/m * (X' * (X * theta - y));
+grad(2:end) += lambda/m * theta(2:end);
 
 % =========================================================================
 
