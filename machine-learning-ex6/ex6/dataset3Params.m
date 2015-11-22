23878915a58e4a396a5b250dfb281fb7eef25d2b
Silviu Apostu <silviu@qlearning.io>
2015-11-22 13:37:25 +0100

    solved ex6 SVM

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+Cvec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmavec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,7 +23,21 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
+for i = 1:size(Cvec,2)
+  for j = 1:size(sigmavec,2)
+    model = svmTrain(X, y, Cvec(i), @(x1, x2) gaussianKernel(x1, x2, sigmavec(j)));
+    predictions = svmPredict(model, Xval);
+    err = mean(double(predictions ~= yval));
+    if i + j == 2
+      errmin = err;
+    endif
+    if (err <= errmin)
+      errmin = err;
+      C = Cvec(i);
+      sigma = sigmavec(j);
+    endif
+  endfor
+endfor
 
 
 
